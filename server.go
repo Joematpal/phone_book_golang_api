1e93ede594a495c0e499ef95dbe5d87b31977bb5
Joseph Palacio <josephmpalacio@gmail.com>
2018-03-23 00:12:14 -0500

    Add app constructor to the server file

---
+++ server.go
@@ -0,0 +1,23 @@
+package server
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error Loading Enironment Variables")
+	}
+	a := App{}
+	a.Initialize(
+		os.Getenv("APP_DB_USERNAME"),
+		os.Getenv("APP_DB_PASSWORD"),
+		os.Getenv("APP_DB_NAME"),
+	)
+
+	a.Run(":8080")
+}
